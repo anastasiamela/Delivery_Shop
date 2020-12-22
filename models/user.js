63bf5254613ebabe5c5c69a021e2f5a04a3a15a9
anastasiamela <anastasia.mela11@gmail.com>
2020-12-22 18:24:20 +0200

    isAdmin optional for signup

--- models/user.js
+++ models/user.js
@@ -40,7 +40,8 @@ function validateUser(user) {
   const schema = Joi.object({
     name: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required()
+    password: Joi.string().min(5).max(255).required(),
+    isAdmin: Joi.boolean().optional()
   });
 
   return schema.validate(user);
