8e514a6209a20bbb5f25caad41f05363ae4dca11
anastasiamela <anastasia.mela11@gmail.com>
2020-12-22 18:05:29 +0200

    admin-seeder --> users-seeder

--- seed/users-seeder.js
+++ seed/users-seeder.js
@@ -7,21 +7,28 @@ mongoose.connect('mongodb://localhost/delivery_shop', { useNewUrlParser: true, u
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
 
-let user = new User({
+let users = [new User({
     name: "Shop Admin Anastasia",
     email: "shop@gmail.com",
     password: "12345",
     isAdmin: true
-});
+}),
+new User({
+    name: "Client Anastasia",
+    email: "anastasia@gmail.com",
+    password: "12345",
+    isAdmin: false
+})];
 
-saveUser(user);
+saveUsers(users);
 
-async function saveUser(user) {
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-    user.save(function (err, result) {
-        exit();
-    });
+async function saveUsers(users) {
+    for (let i = 0; i < users.length; i++) {
+        const salt = await bcrypt.genSalt(10);
+        users[i].password = await bcrypt.hash(users[i].password, salt);
+        await users[i].save();
+    }
+    exit();
 }
 
 function exit() {
