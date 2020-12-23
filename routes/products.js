154c610989eff3c4bcdec59ed4c62f3945e2f929
anastasiamela <anastasia.mela11@gmail.com>
2020-12-23 03:19:48 +0200

    get products with categoryId parameter

--- routes/products.js
+++ routes/products.js
@@ -1,27 +1,42 @@
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const validateObjectId = require('../middleware/validateObjectId')
-const {Product, validate} = require('../models/product'); 
-const {Category} = require('../models/category');
+const { Product, validate } = require('../models/product');
+const { Category } = require('../models/category');
 const mongoose = require('mongoose');
+const Joi = require('joi');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const products = await Product.find().sort('name');
-  res.send(products);
+  const categoryId = req.query.categoryId;
+
+  if (typeof categoryId === 'undefined' || categoryId === '') {
+    const products = await Product.find().sort('name');
+    res.send(products);
+  }
+  else {
+    const { error } = validateParameter({ categoryId: categoryId });
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const category = await Category.findById(categoryId);
+    if (!category) return res.status(400).send('Invalid category.');
+
+    const products = await Product.find({ "category._id": categoryId }).sort('name');
+    res.send(products);
+  }
 });
 
 router.post('/', [auth, admin], async (req, res) => {
-  const { error } = validate(req.body); 
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   console.log(error);
 
   const category = await Category.findById(req.body.categoryId);
   if (!category) return res.status(400).send('Invalid category.');
 
-  const product = new Product({ 
+  const product = new Product({
     name: req.body.name,
     description: req.body.description,
     category: {
@@ -32,19 +47,19 @@ router.post('/', [auth, admin], async (req, res) => {
     price: req.body.price
   });
   await product.save();
-  
+
   res.send(product);
 });
 
 router.put('/:id', [validateObjectId, auth, admin], async (req, res) => {
-  const { error } = validate(req.body); 
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const category = await Category.findById(req.body.categoryId);
   if (!category) return res.status(400).send('Invalid category.');
 
   const product = await Product.findByIdAndUpdate(req.params.id,
-    { 
+    {
       name: req.body.name,
       description: req.body.description,
       category: {
@@ -56,7 +71,7 @@ router.put('/:id', [validateObjectId, auth, admin], async (req, res) => {
     }, { new: true });
 
   if (!product) return res.status(404).send('The product with the given ID was not found.');
-  
+
   res.send(product);
 });
 
@@ -75,5 +90,13 @@ router.get('/:id', validateObjectId, async (req, res) => {
 
   res.send(product);
 });
+
+function validateParameter(req) {
+  const schema = Joi.object({
+    categoryId: Joi.objectId().required()
+  });
+
+  return schema.validate(req);
+}
 
 module.exports = router; 
